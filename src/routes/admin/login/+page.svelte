1c14322d647c016155bb85ebdaeeacb9c48530bc
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-02-09 22:38:24 +0800

    refactor(login-form): refactor login form html code

--- src/routes/admin/login/+page.svelte
+++ src/routes/admin/login/+page.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+
 	export let form;
 </script>
 
 <h1>Admin Login Page</h1>
 <p><code>// TODO: Create login form</code></p>
 
-<form method="POST">
+<form method="POST" use:enhance>
 	<label for="admin_email">
 		<span>Email</span>
 	</label>
-	<input type="email" name="admin_email" id="admin_email" value={form?.data?.admin_email ?? ''} />
+	<input
+		type="email"
+		name="admin_email"
+		id="admin_email"
+		value={form?.data?.admin_email ?? ''}
+		required
+	/>
 	<label for="admin_email">
 		{#if form?.errors?.admin_email}
 			<span class="error">{form?.errors?.admin_email[0]}</span>
@@ -19,7 +27,7 @@
 	<label for="admin_password">
 		<span>Password</span>
 	</label>
-	<input type="password" name="admin_password" id="admin_passwrd" />
+	<input type="password" name="admin_password" id="admin_passwrd" required />
 	<label for="admin_password">
 		{#if form?.errors?.admin_password}
 			<span class="error">{form?.errors?.admin_password[0]}</span>
