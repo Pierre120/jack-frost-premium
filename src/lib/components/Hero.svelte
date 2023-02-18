6f0e9295a42cd084a1c4da23e216d53428e88600
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-02-18 23:33:17 +0800

    add(hero): add hero component in landing page

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import heroA from '$lib/assets/hero/a.png';
+  import heroB from '$lib/assets/hero/b.png';
+  import heroC from '$lib/assets/hero/c.png';
+
+  let Carousel;
+  onMount(async () => {
+    Carousel = (await import('svelte-carousel')).default;
+  });
+
+  let imageSlides = [
+    {
+      alt: 'Jack Frost Premium Ice Cream',
+      src: heroA,
+    },
+    {
+      alt: 'Jack Frost Premium Ice Cream',
+      src: heroB,
+    },
+    {
+      alt: 'Jack Frost Premium Ice Cream',
+      src: heroC,
+    },
+  ];
+</script>
+
+<!-- <Carousel /> -->
+<svelte:component this={Carousel} dots={false} arrows={false} autoplay>
+  {#each imageSlides as img}
+    <img src={img.src} alt={img.alt} class="m-0 p-0 max-w-full" />
+  {/each}
+</svelte:component>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+  import Hero from '$lib/components/Hero.svelte';
+</script>
+
+<Hero />
 <h1 class="text-teal-600">Jack Frost Premium Ice Cream Landing Page</h1>
 <p><code>// TODO: Implement Landing page</code></p>
+
