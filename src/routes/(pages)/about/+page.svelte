4d99c83c26c9246c8244e110a43061040428af23
Thread <airon_cruz@dlsu.edu.ph>
2023-02-19 23:54:25 +0800

    Added title component for contact-page

--- src/routes/(pages)/about/+page.svelte
+++
@@ -1,3 +0,0 @@
-<h1 class="text-teal-600">Jack Frost Premium Ice Cream About Us Page</h1>
-<p><code>// TODO: Implement About Us page</code></p>
-
--- src/routes/(pages)/contact/+page.svelte
+++ src/routes/(pages)/contact/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class="text-teal-600">Jack Frost Premium Ice Cream Contact Page</h1>
-<p><code>// TODO: Implement Contact page</code></p>
+<div class="min-h-screen max-w-screen flex flex-col">
+  <div class="flex-1">
+
+  </div>
+</div>
