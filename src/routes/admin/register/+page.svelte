461f8616e2c9f5ed921be146d79935b8f259017d
Thread <airon_cruz@dlsu.edu.ph>
2023-02-19 12:46:38 +0800

    Added frontend design for admin login

--- src/routes/admin/register/+page.svelte
+++ src/routes/admin/register/+page.svelte
@@ -23,13 +23,13 @@
 				
 				 <!-- <input type="email" class="input input-bordered input-info w-full max-w-xs" id="email" name="email" required />-->
 					<!--<label for="password">Password</label>-->
-					<div>
+					<div class="mb-5">
 						<label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
 						<input type="password" placeholder="Enter your password" class="input input-info input-bordered text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="password" name="password" required />
 					</div>
 					
 					<!--<input type="password" id="password" name="password" required />-->
-					<br>
+					
 					<button class="w-full btn btn-button text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" type="submit">Create an account</button>
 					<!--<button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" type="submit">Register</button>-->
 				</form>
