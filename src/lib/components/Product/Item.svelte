6c0dfdea0869fee8facd1c1013f2c69e3ccf5d35
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-02-19 23:41:01 +0800

    add(item-component): create item component to hold one product

---
+++ src/lib/components/Product/Item.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  const showItem = () => {
+    dispatch("showItem", { productId });
+  };
+
+  export let name: string;
+  export let image: string;
+  export let productId: string;
+</script>
+
+<div class="item-container">
+  <div class="item-image" on:click={showItem}>
+    <img src={image} alt={name} />
+  </div>
+  <div class="item-name">
+    <h3 on:click={showItem}>{name}</h3>
+  </div>
+</div>
+
+<style lang="postcss">
+  .item-container {
+    @apply flex flex-col min-w-min w-full;
+  }
+
+  .item-image {
+    @apply w-full rounded-3xl shadow-md hover:shadow-xl;
+  }
+
+  .item-image > img {
+    @apply w-full object-cover;
+  }
+
+  .item-name {
+    @apply w-full min-w-min font-Itim text-4xl;
+  }
+
+  .item-name > h3 {
+    @apply p-4 text-center text-navy-blue hover:text-dark-navy-blue;
+  }
+</style>
