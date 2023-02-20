156977e79a747bc2fcd10649c44d24891ae59480
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-02-20 16:35:19 +0800

    refactor(contact-page-layout): refactor layout for contact page

--- src/routes/(pages)/order/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import type { LayoutData } from '../$types';
-  import Footer from "$lib/components/Footer/index.svelte";
-  export let data: LayoutData;
-</script>
-
-<slot />
-<Footer data={data}/>
