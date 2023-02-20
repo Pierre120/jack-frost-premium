6c8e910c181202553f88dfb6c8e9307bedc1780d
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-02-20 14:56:44 +0800

    add(product-types): add product typescript types in library

---
+++ src/lib/types/product.d.ts
@@ -0,0 +1,13 @@
+export interface Offering {
+  amount: string;
+  other_amt: string;
+  price: number;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  img_src: string;
+  description: string;
+  offerings: Offering[];
+}
