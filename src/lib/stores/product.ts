0db0d5f9779120e7e3198e7dd2f7bbfdc523634a
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-02-27 00:28:19 +0800

    refactor(product-store): refactor definition of product store

--- src/lib/stores/product.ts
+++ src/lib/stores/product.ts
@@ -2,6 +2,12 @@ import type { Product } from "$lib/types/product";
 import { writable } from "svelte/store";
 
 // This is a store that holds the product state
-const ProductStore = writable({} as Product);
+const ProductStore = writable<Product>({
+  id: "",
+  name: "",
+  description: "",
+  img_src: "",
+  offerings: [],
+});
 
 export default ProductStore;
