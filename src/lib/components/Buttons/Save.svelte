20b1ebe5afcaef60719801a2ce2f5b50438b839e
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-08 03:10:36 +0800

    chore(format): format using prettier

--- src/lib/components/Buttons/Save.svelte
+++ src/lib/components/Buttons/Save.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	export let form: string;
 	export let formaction: string;
 	export let label = 'Save';
 </script>
 
-<button class="save-btn" type="submit" {formaction}>
+<button class="save-btn" type="submit" {form} {formaction}>
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon">
 		<!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 		<path
