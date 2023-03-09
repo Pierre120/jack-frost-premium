658df6422cc00aabb22789c5f8269f816aab559c
Thread <airon_cruz@dlsu.edu.ph>
2023-03-09 10:54:24 +0800

    Added session validation for category-page

---
+++ src/routes/(admin)/admin/categories/add/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import AddCategoryForm from '$lib/components/Forms/Category.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<AddCategoryForm label="Add Category" formaction="/api/category/add" categories={data.categories} />
