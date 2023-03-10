20d20f03834a4f81535e916291c1fb2117249915
Thread <airon_cruz@dlsu.edu.ph>
2023-03-10 10:07:48 +0800

    feat(edit-categories)

---
+++ src/routes/(admin)/admin/categories/edit/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import EditCategoryForm from '$lib/components/Forms/Category.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<EditCategoryForm label="Edit Category" formaction="/api/category/edit" categories={data.categories} hasDeleteButton= {true} />
