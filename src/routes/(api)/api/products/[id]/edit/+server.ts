b46bace910b38146ad11240cc36246fd5661a197
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-13 11:26:49 +0800

    feat(edit-product-api): create API endpoint for editing a product

---
+++ src/routes/(api)/api/products/[id]/edit/+server.ts
@@ -0,0 +1,25 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { editProduct } from '$lib/server/products/edit';
+
+// Edit a product in the database
+export const POST = (async ({ params, request }) => {
+  try {
+    const { name, is_seasonal, category_id, img_path, img_src, description } = await request.json();
+    const product = await editProduct(params.id ?? '', {
+      name,
+      is_seasonal,
+      category_id,
+      img_path,
+      img_src,
+      description
+    });
+    return json({
+      success: true,
+      product
+    });
+  } catch (err) {
+    console.error(err);
+    return json({ success: false });
+  }
+}) as RequestHandler;
