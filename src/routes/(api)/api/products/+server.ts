6683a7eb0455b1b53cb13b21e01f2e362727159e
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-13 10:58:41 +0800

    feat(get-products-api): add API endpoint for getting all products

---
+++ src/routes/(api)/api/products/+server.ts
@@ -0,0 +1,19 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { getAllProducts } from '$lib/server/products/get';
+
+// Get all products from the database
+export const GET = (async () => {
+  try {
+    const products = await getAllProducts();
+    console.log('From API:');
+    console.log(products);
+    return json({
+      success: true,
+      products
+    });
+  } catch (err) {
+    console.error(err);
+    return json({ success: false });
+  }
+}) satisfies RequestHandler;
