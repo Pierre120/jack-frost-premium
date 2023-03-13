6b88921384f7542165816efb2bdfbc07badc54e7
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-13 11:14:05 +0800

    feat(get-product-api): create API endpoint for getting a product

---
+++ src/routes/(api)/api/products/[id]/+server.ts
@@ -0,0 +1,17 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { getProduct } from '$lib/server/products/get';
+
+// Get a product from the database
+export const GET = (async ({ params }) => {
+  try {
+    const product = await getProduct(params.id ?? '');
+    return json({
+      success: true,
+      product
+    });
+  } catch (err) {
+    console.error(err);
+    return json({ success: false });
+  }
+}) satisfies RequestHandler;
