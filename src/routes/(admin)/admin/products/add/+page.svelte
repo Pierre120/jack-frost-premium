b2445de654d7528be732b2c761026fff767bda70
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-14 01:08:54 +0800

    feat(status-modal): create status modal component

--- src/routes/(admin)/admin/products/add/+page.svelte
+++ src/routes/(admin)/admin/products/add/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import { goto } from '$app/navigation';
+  import { goto, invalidateAll } from '$app/navigation';
 	import AddProductForm from '$lib/components/Forms/Product.svelte';
 	import LeaveConfirmationModal from '$lib/components/Modal/Confirmation.svelte';
+	import StatusModal from '$lib/components/Modal/Status.svelte'
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -11,6 +12,7 @@
 	const cancelLabel = 'Stay on this Page';
 	const confirmLabel = 'Leave this Page';
 	let isAboutToLeave = false;
+	let success = false;
 
 	const discardProduct = () => {
 		isAboutToLeave = true;
@@ -20,8 +22,14 @@
 		isAboutToLeave = false;
 	}
 
-	const confirmDiscard = () => {
-		// isAboutToLeave = false;
+	const confirmDiscard = async () => {
+		isAboutToLeave = false;
+		success = true;
+		await invalidateAll();
+		let start = Date.now();
+		while(true) {
+			if (Date.now() - start > 2000) break;
+		}
 		goto('/admin/products', {invalidateAll: true});
 	}
 </script>
@@ -29,4 +37,7 @@
 <AddProductForm label="Add Product" formaction="/api/products/add" categories={data.categories} on:close={discardProduct} />
 {#if isAboutToLeave}
 	<LeaveConfirmationModal {confirmationHeader} {confirmationDetails} {cancelLabel} {confirmLabel} on:cancel={cancelDiscard} on:confirm={confirmDiscard} />
+{/if}
+{#if success}
+	<StatusModal {success} statusHeader="Product Added" statusInfo="Successfully added product" />
 {/if}
