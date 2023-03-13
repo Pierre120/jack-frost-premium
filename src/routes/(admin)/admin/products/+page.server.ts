f5c6c02ba70bda3dc2f9e2ac0b7dbc7b8e6e7e8b
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-13 22:27:21 +0800

    feat(products-page-server-load): add fetching of products from database to pass to the page

--- src/routes/(admin)/admin/products/+page.server.ts
+++ src/routes/(admin)/admin/products/+page.server.ts
@@ -1,11 +1,20 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { supabase } from '$lib/utils/supabase';
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load = (async ({ locals }) => {
 	const session = await locals.validate();
 	if (!session) {
 		// throw error(401, 'Unauthorized user.'); // 401 error page
 		throw redirect(303, '/'); // Redirect landing page
 	}
-};
+
+	const res = await fetch('/api/products');
+	const products = await res.json();
+	console.log(products); // for debugging purposes
+	if (products.success) {
+		return {
+			products: products.products
+		};
+	}
+	throw error(404, 'Products not found');
+}) satisfies PageServerLoad;
--- src/routes/(admin)/admin/products/+page.svelte
+++ src/routes/(admin)/admin/products/+page.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import AddProduct from '$lib/components/Buttons/Add.svelte';
+	import ProductList from '$lib/components/Product/Admin/List.svelte';
+  import type { PageData } from './$types';
+
+	export let data: PageData
 
 	const addProduct = () => {
 		goto('/admin/products/add');
+	};
+
+	const editProduct = (event: CustomEvent) => {
+		goto(`/admin/products/edit/${event.detail.product_id}`);
 	};
 </script>
 
 <div class="block">
 	<AddProduct label="Add Product" on:add={addProduct} />
 </div>
-<!-- TODO: Product list here -->
+<ProductList products={data.products} on:edit={editProduct} />
