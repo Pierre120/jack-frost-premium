945386a112a036591a632b1da50407e30ef79912
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-14 15:51:52 +0800

    refactor(product-add-page-load): fetch categories from database

--- src/routes/(admin)/admin/products/add/+page.server.ts
+++ src/routes/(admin)/admin/products/add/+page.server.ts
@@ -1,20 +1,25 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ locals }) => {
+export const load = (async ({ locals, fetch }) => {
 	const session = await locals.validate();
 	if (!session) {
 		// throw error(401, 'Unauthorized user.'); // 401 error page
 		throw redirect(303, '/'); // Redirect landing page
 	}
 
-	const categs = [
-		{ id: '1', name: 'Platinum' },
-		{ id: '2', name: 'Premium' },
-		{ id: '3', name: 'Super Premium' }
-	];
+	// const categs = [
+	// 	{ id: '1', name: 'Platinum' },
+	// 	{ id: '2', name: 'Premium' },
+	// 	{ id: '3', name: 'Super Premium' }
+	// ];
+	const res = await fetch('/api/categories');
+	const categories = await res.json();
 
-	return {
-		categories: categs
-	};
+	if(categories.success) {
+		return {
+			categories: categories.categories
+		};
+	}
+	throw error(404, 'Categories not found');
 }) satisfies PageServerLoad;
