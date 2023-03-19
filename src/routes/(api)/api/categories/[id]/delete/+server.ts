261d25c8495f0e24942f3b99e0adf7af65a693a7
Thread <airon_cruz@dlsu.edu.ph>
2023-03-19 19:21:48 +0800

    fix(delete-categ)

--- src/routes/(api)/api/categories/[id]/delete/+server.ts
+++ src/routes/(api)/api/categories/[id]/delete/+server.ts
@@ -3,14 +3,14 @@ import type { RequestHandler } from './$types';
 import { deleteCategory } from '$lib/server/categories/delete';
 
 // Delete a category from the database
-export const POST = (async ({ params }) => {
+export const POST = (async ({ params,fetch }) => {
 	try {
 		const categoryRes = await fetch(`/api/categories/${params.id}`);
 		let category = await categoryRes.json();
 		console.log('In delete category', category);
 
 		category = await deleteCategory(params.id ?? '');
-		return json({ success: true, category });
+		return json({ success: true});
 	} catch (err) {
 		console.error(err);
 		return json({ success: false });
