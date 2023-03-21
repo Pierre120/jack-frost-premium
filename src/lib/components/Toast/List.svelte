317e4d54b8949942463170efe7af0bf5bba26f72
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-21 22:10:07 +0800

    refactor(toasts-component): modify padding value

--- src/lib/components/Toast/List.svelte
+++ src/lib/components/Toast/List.svelte
@@ -16,7 +16,7 @@
 
 <style lang="postcss">
   section {
-    @apply fixed top-0 left-0 right-0 w-full flex flex-col gap-y-4 p-4
+    @apply fixed top-0 left-0 right-0 w-full flex flex-col gap-y-4 p-10
       items-center justify-start z-40;
   }
 </style>
