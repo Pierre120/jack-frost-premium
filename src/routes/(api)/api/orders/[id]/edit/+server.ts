bcfa6c0c1d10710d884c0684c2f9777abf26e2fe
Thread <airon_cruz@dlsu.edu.ph>
2023-03-23 00:10:39 +0800

    feat(edit-order)

---
+++ src/routes/(api)/api/orders/[id]/edit/+server.ts
@@ -0,0 +1,23 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { editOrder} from '$lib/server/orders/edit';
+
+
+// Edit an order in the database
+export const POST = (async ({ params, request }) => {
+	try {
+    //TODO :  add variables here
+		const { estimated_delivery, payment_status, } = await request.json();
+		const order = await editOrder(params.id ?? '', {
+			estimated_delivery,
+      payment_status
+		});
+		return json({
+			success: true,
+			order
+		});
+	} catch (err) {
+		console.error(err);
+		return json({ success: false });
+	}
+}) satisfies RequestHandler;
