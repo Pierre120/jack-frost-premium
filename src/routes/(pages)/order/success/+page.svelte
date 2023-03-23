e7301fae96e9dea00dbf08fdc80d7297da52a9d1
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-24 00:12:31 +0800

    wip(order-pages): fix container

--- src/routes/(pages)/order/success/+page.svelte
+++ src/routes/(pages)/order/success/+page.svelte
@@ -6,7 +6,7 @@
 	<title>Order Success! | Jack Frost Premium Ice Cream</title>
 </svelte:head>
 
-<div class="bg-white">
+<div class="w-full min-h-screen bg-white flex items-start justify-center py-4">
 	<h1 class="text-teal-600">Jack Frost Premium Ice Cream Order Page</h1>
 	<p><code>// TODO: Implement Order page</code></p>
 </div>
