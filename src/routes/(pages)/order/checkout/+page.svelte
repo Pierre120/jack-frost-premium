a9d9a652a6aa4354463c891bdc47a21fd5d689af
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-24 00:04:24 +0800

    feat(order-pages): add /order/checkout and /order/success pages

---
+++ src/routes/(pages)/order/checkout/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { retrieveCart } from '$lib/stores/cart';
+</script>
+
+<svelte:head>
+	<title>Order Checkout! | Jack Frost Premium Ice Cream</title>
+</svelte:head>
+
+<div class="bg-white">
+	<h1 class="text-teal-600">Jack Frost Premium Ice Cream Order Page</h1>
+	<p><code>// TODO: Implement Order page</code></p>
+</div>
+<ul>
+	{#each retrieveCart().items as item}
+		<li>{item.product.name}</li>
+	{/each}
+</ul>
