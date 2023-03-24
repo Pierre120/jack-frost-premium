bec38e98cc37ddbe8477bdc6ff35dc28cf428f9c
Pierre Hernandez <pierre_hernandez@dlsu.edu.ph>
2023-03-24 18:50:26 +0800

    rename order/success path and page to order/confirmattion path and page

---
+++ src/routes/(pages)/order/confirmation/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { retrieveCart } from '$lib/stores/cart';
+</script>
+
+<svelte:head>
+	<title>Order Confirmation | Jack Frost Premium Ice Cream</title>
+</svelte:head>
+
+<div class="w-full min-h-screen bg-white flex items-start justify-center py-4">
+	<h1 class="text-teal-600">Jack Frost Premium Ice Cream Order Page</h1>
+	<p><code>// TODO: Implement Order page</code></p>
+	
+</div>
+
