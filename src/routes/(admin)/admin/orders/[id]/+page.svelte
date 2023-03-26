d941a012264ed528a75b4a9907507e207a6e2223
Thread <airon_cruz@dlsu.edu.ph>
2023-03-27 02:03:34 +0800

    Added 'editOrder' form

--- src/routes/(admin)/admin/orders/[id]/+page.svelte
+++ src/routes/(admin)/admin/orders/[id]/+page.svelte
@@ -4,6 +4,101 @@ import OrderForm from '$lib/components/Forms/Order/index.svelte';
 import type { Order } from '$lib/types/order';
 import type { SubmitFunction } from '$app/forms';
 import type { ActionData } from './$types';
+import StatusModal from '$lib/components/Modal/Status.svelte';
+
+export let data: PageData;
+
+let confirmationHeader = '';
+let confirmationDetails = '';
+let cancelLabel = '';
+let confirmLabel = '';
+let statusHeader = '';
+let statusInfo = '';
+let isAboutToLeave = false;
+let isAboutToDelete = false;
+let success = false;
+let deleted = false;
+let loading = false;
+
+const successEdit = async () => {
+		success = true;
+		deleted = false;
+		statusHeader = 'CHANGES SAVED';
+		statusInfo = 'The changes you made have been saved';
+		// await invalidateAll();
+	};
+
+	const successDelete = async () => {
+		success = false;
+		deleted = true;
+		isAboutToDelete = false;
+		statusHeader = 'PRODUCT DELETED';
+		statusInfo = 'The product has been deleted';
+		// await invalidateAll();
+		setTimeout(() => {
+			goto('/admin/products');
+		}, 1500);
+	};
+
+	const discardChange = () => {
+		confirmationHeader = 'DISCARD CHANGES?';
+		confirmationDetails =
+			"The changes you've made will not be saved if you leave this page without saving.";
+		cancelLabel = 'Stay on this Page';
+		confirmLabel = 'Leave this Page';
+		isAboutToLeave = true;
+	};
+
+	const deleteProduct = () => {
+		confirmationHeader = 'DELETE PRODUCT?';
+		confirmationDetails = 'Are you sure you would like to delete this product?';
+		cancelLabel = 'Cancel';
+		confirmLabel = 'Delete Product';
+		isAboutToDelete = true;
+	};
+
+	const cancel = () => {
+		isAboutToLeave = false;
+		isAboutToDelete = false;
+	};
+
+	const confirm = async () => {
+		if (isAboutToLeave) {
+			isAboutToLeave = false;
+			goto('/admin/products');
+		}
+		if (isAboutToDelete) {
+			loading = true;
+			statusHeader = 'FOR A MOMENT...';
+			statusInfo = 'Deleting the product...';
+			// Delete
+			const result = await fetch(`/api/products/${data.product.id}/delete`, {
+				method: 'POST'
+			});
+			if ((await result.json()).success) {
+				loading = false;
+				await successDelete();
+			}
+		}
+	};
+
+	const submitEdit: SubmitFunction = async () => {
+		loading = true;
+		statusHeader = 'FOR A MOMENT...';
+		statusInfo = 'Saving the changes...';
+		return async ({ result, update }) => {
+			switch (result.type) {
+				case 'redirect':
+					loading = false;
+					await successEdit();
+					break;
+				case 'error':
+					console.log(result.error);
+					break;
+			}
+			await update();
+		};
+	};
 </script>
 
 <svelte:head>
